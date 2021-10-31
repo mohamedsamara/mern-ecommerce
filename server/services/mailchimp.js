71f522a5c86668c82977d05bd97a30ea41b62d15
Mohamed Samara <45723974+mohamedsamara@users.noreply.github.com>
2021-10-31 02:19:37 -0700

    feat: add fix to api missing keys

--- server/services/mailchimp.js
+++ server/services/mailchimp.js
@@ -1,18 +1,28 @@
-const mailchimp = require('../config/mailchimp');
+const Mailchimp = require('mailchimp-api-v3');
+
+const keys = require('../config/keys');
+
+const { key, listKey } = keys.mailchimp;
+
+class MailchimpService {
+  init() {
+    try {
+      return new Mailchimp(key);
+    } catch (error) {
+      console.warn('Missing mailgun keys');
+    }
+  }
+}
+
+const mailchimp = new MailchimpService().init();
 
 exports.subscribeToNewsletter = async email => {
-  let result = {};
-  let response;
-
   try {
-    response = await mailchimp.subscribeToNewsletter(email);
+    return await mailchimp.post(`lists/${listKey}/members`, {
+      email_address: email,
+      status: 'subscribed'
+    });
   } catch (error) {
     return error;
   }
-
-  if (response) {
-    result = response;
-  }
-
-  return result;
 };
