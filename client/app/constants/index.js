50a0757682630c07aea558a5891b3d88d2498bae
Alejo <sagaalejo@gmail.com>
2023-10-24 14:43:44 -0300

    Fixes varios a .env y a la refurl de BASEAPIURL

--- client/app/constants/index.js
+++ client/app/constants/index.js
@@ -2,7 +2,7 @@ export const API_URL = process.env.API_URL;
 
 export const SOCKET_URL =
   window.location.host.indexOf('localhost') >= 0
-    ? 'http://127.0.0.1:3000'
+    ? 'http://127.0.0.1:3001'
     : window.location.host;
 
 export const ROLES = {
