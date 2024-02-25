cf992c8ffc1c86402fb4a3b6a2f8b8d97f1a9d99
mansunsilwal77 <mansun.silwal@gmail.com>
2024-02-25 10:44:48 +0545

    final deploy
    expect added

--- .github/IaC/main.tf
+++ .github/IaC/main.tf
@@ -71,7 +71,7 @@ output "public_ip" {
   value = aws_instance.mern-instance.public_ip
 }
 output "private_ip" {
-  value = aws_instance.mern-instance.private_ip
+  value = aws_instance.mern-instance.private_ip 
 }
 
 
