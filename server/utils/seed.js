595e75b7748893257da084196deb31240013eccb
Pete Cheslock <petecheslock@gmail.com>
2024-07-09 14:03:07 -0400

    Update DB seed to run in a container, and create categories and associated products

--- server/utils/seed.js
+++ server/utils/seed.js
@@ -8,23 +8,26 @@ const { ROLES } = require('../constants');
 const User = require('../models/user');
 const Brand = require('../models/brand');
 const Product = require('../models/product');
+const Category = require('../models/category');
 
 const args = process.argv.slice(2);
 const email = args[0];
 const password = args[1];
 
+const NUM_PRODUCTS = 100;
+const NUM_BRANDS = 10;
+const NUM_CATEGORIES = 10;
+
 const seedDB = async () => {
   try {
+    let categories = [];
+
     console.log(`${chalk.blue('✓')} ${chalk.blue('Seed database started')}`);
 
     if (!email || !password) throw new Error('Missing arguments');
-
-    // Check if admin user exists
     const existingUser = await User.findOne({ email });
     if (!existingUser) {
       console.log(`${chalk.yellow('!')} ${chalk.yellow('Seeding admin user...')}`);
-
-      // Seed admin user
       const user = new User({
         email,
         password,
@@ -36,42 +39,67 @@ const seedDB = async () => {
       const salt = await bcrypt.genSalt(10);
       const hash = await bcrypt.hash(user.password, salt);
       user.password = hash;
-
       await user.save();
       console.log(`${chalk.green('✓')} ${chalk.green('Admin user seeded.')}`);
     } else {
       console.log(`${chalk.yellow('!')} ${chalk.yellow('Admin user already exists, skipping seeding for admin user.')}`);
     }
 
-    // Seed brands
-    for (let i = 0; i < 10; i++) {
-      const brand = new Brand({
-        name: faker.company.name(),
-        description: faker.lorem.sentence(),
-        isActive: true
-      });
-
-      await brand.save();
+    const categoriesCount = await Category.countDocuments();
+    if (categoriesCount >= NUM_CATEGORIES) {
+      console.log(`${chalk.yellow('!')} ${chalk.yellow('Sufficient number of categories already exist, skipping seeding for categories.')}`);
+      categories = await Category.find().select('_id');
+    } else {
+      for (let i = 0; i < NUM_CATEGORIES; i++) {
+        const category = new Category({
+          name: faker.commerce.department(),
+          description: faker.lorem.sentence(),
+          isActive: true
+        });
+        await category.save();
+        categories.push(category);
+      }
+      console.log(`${chalk.green('✓')} ${chalk.green('Categories seeded.')}`);
     }
-    console.log(`${chalk.green('✓')} ${chalk.green('Brands seeded.')}`);
 
-    // Seed products
-    const brands = await Brand.find().select('_id');
-    for (let i = 0; i < 50; i++) {
-      const product = new Product({
-        sku: faker.string.alphanumeric(10),
-        name: faker.commerce.productName(),
-        description: faker.lorem.sentence(),
-        quantity: faker.number.int({ min: 1, max: 100 }),
-        price: faker.commerce.price(),
-        taxable: faker.datatype.boolean(),
-        isActive: true,
-        brand: brands[faker.number.int(brands.length - 1)]._id
-      });
-
-      await product.save();
+    const brandsCount = await Brand.countDocuments();
+    if (brandsCount >= NUM_BRANDS) {
+      console.log(`${chalk.yellow('!')} ${chalk.yellow('Sufficient number of brands already exist, skipping seeding for brands.')}`);
+    } else {
+      for (let i = 0; i < NUM_BRANDS; i++) {
+        const brand = new Brand({
+          name: faker.company.name(),
+          description: faker.lorem.sentence(),
+          isActive: true
+        });
+        await brand.save();
+      }
+      console.log(`${chalk.green('✓')} ${chalk.green('Brands seeded.')}`);
     }
-    console.log(`${chalk.green('✓')} ${chalk.green('Products seeded.')}`);
+
+    const productsCount = await Product.countDocuments();
+    if (productsCount >= NUM_PRODUCTS) {
+      console.log(`${chalk.yellow('!')} ${chalk.yellow('Sufficient number of products already exist, skipping seeding for products.')}`);
+    } else {
+      const brands = await Brand.find().select('_id');
+      for (let i = 0; i < NUM_PRODUCTS; i++) {
+        const randomCategoryIndex = faker.number.int(categories.length - 1);
+        const product = new Product({
+          sku: faker.string.alphanumeric(10),
+          name: faker.commerce.productName(),
+          description: faker.lorem.sentence(),
+          quantity: faker.number.int({ min: 1, max: 100 }),
+          price: faker.commerce.price(),
+          taxable: faker.datatype.boolean(),
+          isActive: true,
+          brand: brands[faker.number.int(brands.length - 1)]._id,
+          category: categories[randomCategoryIndex]._id
+        });
+        await product.save();
+        await Category.updateOne({ _id: categories[randomCategoryIndex]._id }, { $push: { products: product._id } });
+      }
+      console.log(`${chalk.green('✓')} ${chalk.green('Products seeded and associated with categories.')}`);
+    }
 
   } catch (error) {
     console.log(`${chalk.red('x')} ${chalk.red('Error while seeding database')}`);
