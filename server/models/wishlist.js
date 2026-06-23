2dc0c168ca3b42d1730c614630764acb5715be28
DefNotArham <arhamkabir231@gmail.com>
2026-06-22 21:23:53 -0400

    improved wishlist model

--- server/models/wishlist.js
+++ server/models/wishlist.js
@@ -2,29 +2,30 @@ const Mongoose = require('mongoose');
 const { Schema } = Mongoose;
 
 // Wishlist Schema
-const WishlistSchema = new Schema({
-  product: {
-    type: Schema.Types.ObjectId,
-    ref: 'Product',
-    default: null
+const WishlistSchema = new Schema(
+  {
+    product: {
+      type: Schema.Types.ObjectId,
+      ref: 'Product',
+      default: null,
+      required: true
+    },
+    user: {
+      type: Schema.Types.ObjectId,
+      ref: 'User',
+      default: null,
+      required: true
+    },
+    isLiked: {
+      type: Boolean,
+      default: false
+    },
+    updated: {
+      type: Date,
+      default: Date.now
+    }
   },
-  user: {
-    type: Schema.Types.ObjectId,
-    ref: 'User',
-    default: null
-  },
-  isLiked: {
-    type: Boolean,
-    default: false
-  },
-  updated: {
-    type: Date,
-    default: Date.now
-  },
-  created: {
-    type: Date,
-    default: Date.now
-  }
-});
+  { timestamps: true }
+);
 
 module.exports = Mongoose.model('Wishlist', WishlistSchema);
